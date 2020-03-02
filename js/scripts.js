8d551116ad5744033c7d6871291a3b26575e6965
Dan Njoroge <greatdaniels@gmail.com>
2020-03-02 13:02:07 +0300

    adding a pizza

--- js/scripts.js
+++ js/scripts.js
@@ -82,7 +82,68 @@ $(document).ready(function(){
         $("#pizzatopping").html(pizzaTopping.join(", "));
         $("#totals").html(total);
 
+        // Adding a pizza 
+        $("button.addPizza").click(function(){
+            let pizzaName = $(".name option:selected").val();
+            let pizzaSize = $("#size option:selected").val();
+            let pizzaCrust = $("#crust option:selected").val();
+            let pizzaTopping = [];
+            $.each($("input[name='toppings']:checked"), function(){            
+                pizzaTopping.push($(this).val());
+            });
+            console.log(pizzaTopping.join(", "));
         
+            switch(pizzaSize){
+            case "0":
+                price =0;
+            break;
+            case "large":
+                price = 800;
+                console.log(price);
+            break;
+            case "medium":
+                price = 600;
+                console.log("The price is: "+price);
+            break;
+            case "small":
+                price = 400;
+                console.log(price);
+            default:
+                console.log("Select Pizza size"); 
+            }
+  
+            switch(pizzaCrust){
+                case "0":
+                crustPrice = 0;
+                break;
+                case "Crispy":
+                crustPrice = 200;
+                break;
+                case "Stuffed":
+                crustPrice = 150;
+                break;
+                case "Gluten-free":
+                crustPrice = 180;
+                break;
+                default:
+                console.log("Select Pizza Crust"); 
+            }
+  
+            let topping_value = pizzaTopping.length*100;
+            console.log("Pizza Toppings value :" + topping_value);
+            total = price + crustPrice + topping_value;
+            console.log(total);
+    
+            checkoutTotal = checkoutTotal + total;
+            console.log(checkoutTotal);
+  
+            // new Object
+            var newOrder = new Orderpizza(pizzaSize, pizzaCrust,pizzaTopping,total);
+  
+            $("#ordersmade").append('<tr><td id="pizzasize">' + newOrder.size + '</td><td id="pizzacrust">'+newOrder.crust + '</td><td id="pizzatopping">'+newOrder.topping+'</td><td id="totals">'+newOrder.total+'</td></tr>');
+            console.log(newOrder);
+      
+        });
 
 
     })
